87b526f430b53fa6361c09af4cbfdaa3078e8b3e
unknown <Administrator@Moulali.MOULA>
2019-05-15 17:56:56 +0530

    initial commit

---
+++ src/test/java/com/company/app/myApp/AppTest.java
@@ -0,0 +1,18 @@
+package com.company.app.myApp;
+
+import org.testng.annotations.Test;
+
+public class AppTest 
+{
+	@Test
+    public void check()
+    {
+        System.out.println( "1. Hello World!" );
+    }
+	
+	@Test
+    public void check2()
+    {
+        System.out.println( "2. Hello World!" );
+    }
+}
